cffff833fa71a43cc99bba798cf12181e37acb5d
Gabrielly <gabriellysb40028922@gmail.com>
2024-04-19 23:58:49 -0300

    feat: add ipynb for better visualization about data with the  graphs

--- src/config/aws_config.py
+++ src/config/aws_config.py
@@ -9,9 +9,14 @@ BUCKET_NAME = os.environ.get("BUCKET_NAME")
 DATABASE_NAME = os.environ.get("DATABASE_NAME")
 AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+BUCKET_KEY_HOSPITAL_BEDS = os.environ.get("BUCKET_KEY_HOSPITAL_BEDS")
+BUCKET_KEY_CASES_AND_DEATHS = os.environ.get("BUCKET_KEY_CASES_AND_DEATHS")
+BUCKET_KEY_GLOBAL_DEATHS = os.environ.get("BUCKET_KEY_GLOBAL_DEATHS")
+BUCKET_KEYS_VACCINATED_PER_HUNDRED = os.environ.get("BUCKET_KEYS_VACCINATED_PER_HUNDRED")
+BUCKET_KEY_DOSES_PFIZER = os.environ.get("BUCKET_KEY_DOSES_PFIZER")
 
 session = boto3.Session(
     region_name=REGION_NAME,
     aws_access_key_id=AWS_ACCESS_KEY,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
 )
